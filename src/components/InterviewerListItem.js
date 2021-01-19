20f4ace4a922f07ebff37293a8bb6a0b0c39e9f0
firebfm <firebfm@gmail.com>
2021-01-19 15:01:18 -0500

    build InterviewerListItem, storybook look

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,21 @@
 import React from "react";
+import classnames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-
+  const interviewerItemClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item-image": props.avatar
+  });
 
   return (
-    <li className="interviewers__item">
+    <li className={interviewerItemClass} onClick={() => props.setInterviewer(props.name)}>
     <img
-      className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      className={"interviewers__item-image"}
+      src={props.avatar}
+      alt={props.name}
     />
-    Sylvia Palmer
+    {props.selected && props.name}
   </li>
   );
 }
