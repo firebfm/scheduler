8ac063a7ab45b3a2650118efd36c1fbed88255d2
firebfm <firebfm@gmail.com>
2021-01-18 22:58:39 -0500

    testing daylistitem, formatSpots()

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  // destructure
+  // const {name, spots} = props;
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    } else if (props.spots === 1) {
+      return '1 spot remaining'
+      } else if (props.spots) {
+        return `${props.spots} spots remaining`
+      }
+    };
+
   return (
     <li className={dayItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
