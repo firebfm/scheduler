6ddf52945f82c74738535f1dcb6898c6dffaea1f
simon <firebfm@gmail.com>
2021-01-20 15:46:57 -0500

    add appointments data to application, show schedule with map and spreading props

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,54 @@
 import React, { useState } from "react";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 import "components/Application.scss";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Hana Kuk",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4:30pm",
+    interview: {
+      student: "Morty Smith",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
 
 const days = [
   {
@@ -45,7 +93,12 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          console.log(appointments)
+          // <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview}/>
+          return <Appointment key={appointment.id} {...appointment}/>
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
