6e4c8f808bbe887fc5b2d68fe4897b4782d9da23
simon <firebfm@gmail.com>
2021-01-25 22:07:46 -0500

    add submit name save test in form test

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -16,10 +16,6 @@ describe("Form", () => {
     }
   ];
 
-  it("renders without crashing", () => {
-    render(<Form />);
-  });  
-
   it("renders without student name if not provided", () => {
     const { getByPlaceholderText } = render(
       <Form interviewers={interviewers} />
@@ -63,8 +59,23 @@ describe("Form", () => {
     );
     /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
-    
+
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+  
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+  
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
