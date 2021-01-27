dbc39ba8c966c635c2712b2a66827b8b9d691b83
simon <firebfm@gmail.com>
2021-01-26 19:08:42 -0500

    integration testing, mock axios put, bug found

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,38 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // console.log(prettyDOM(container));
+
+    const appointments = getAllByTestId(container, "appointment");
+    // console.log("SECOND CONSOLELOG" + prettyDOM(appointments));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+    // console.log("0 index is " + prettyDOM(appointment));
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Tori Malcolm")); // Tori Malcolm is found but Sylvia Palmer cannot be found
+    fireEvent.click(getByText(appointment, "Save"));
+    // If the alt tag, placeholder text or button text is different for your project, you will need to update it to match
+  });
 });
